bf474e4ef91c39a506c04cdc314379b0b716b67e
Kevin Pezoa G <kevin.pezoa@gmail.com>
2023-05-02 23:23:39 -0400

    cambios para pre-entrega 2

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-// Objeto
+// objeto con precios y variedades de café
 const preciosCafe = {
   "americano": 1900,
   "expresso": 1200,
@@ -6,7 +6,8 @@ const preciosCafe = {
   "latte": 2800
 };
 
-let ordenCliente = {};
+// array vacío
+let ordenesClientes = [];
 
 // Funcion y promp
 while (true) {
@@ -16,14 +17,28 @@ while (true) {
   }
   let cantidadCafe = parseInt(prompt(`¿Cuántos quieres ordenar?`));
   if (!isNaN(cantidadCafe)) {
-    ordenCliente[tipoCafe] = cantidadCafe;
+    let nombreCliente = prompt("¿Cuál es tu nombre?");
+    ordenesClientes.push([nombreCliente, tipoCafe, cantidadCafe]);
   }
 }
 
-// Ciclo
-let precioTotal = 0;
-for (let tipoCafe in ordenCliente) {
-  precioTotal += preciosCafe[tipoCafe] * ordenCliente[tipoCafe];
-}
+// map y array
+const preciosOrdenes = ordenesClientes.map(orden => {
+  const precioCafe = preciosCafe[orden[1]];
+  const precioTotal = precioCafe * orden[2];
+  return {
+    // datos ingresados en el promp
+    cliente: orden[0],
+    tipoCafe: orden[1],
+    cantidad: orden[2],
+    precioTotal: precioTotal
+  };
+});
 
-console.log(`El precio total es: $${precioTotal}`);
+// mensaje con el detalle y precio final
+preciosOrdenes.forEach(orden => {
+  console.log(`Gracias ${orden.cliente}, el precio total por ${orden.cantidad} ${orden.tipoCafe} es: $${orden.precioTotal}`);
+});
+
+// console.log para verificar que funcione la info del .map
+console.log(preciosOrdenes)
