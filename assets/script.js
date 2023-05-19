8245025a1e843ca795a5b4e984f49d0e2343927d
Kevin Pezoa G <kevin.pezoa@gmail.com>
2023-05-18 21:43:27 -0400

    se sube cambios de html, csss y js para la tercera pre-entrega. Contiene cambios de prodyctos, diseño, estuctura y funcionalidad con javasscript

--- assets/script.js
+++ assets/script.js
@@ -1,87 +1,178 @@
-// objeto con precios y variedades de café
-const cafeTipos = [
-  {
-    nombre: "Americano",
-    descripcion: "Café negro con agua caliente",
-    precio: 1900,
-    disponible: true,
-  },
-  {
-    nombre: "Expresso",
-    descripcion: "Café negro",
-    precio: 1200,
-    disponible: true,
-  },
-  {
-    nombre: "Cappuccino",
-    descripcion: "Café con leche caliente y espuma de leche",
-    precio: 2600,
-    disponible: false,
-  },
-  {
-    nombre: "Latte",
-    descripcion: "Café con leche caliente y espuma",
-    precio: 2800,
-    disponible: true,
-  },
-];
+let sPhoneData;
+let carritoData = [];
 
-// array vacío
-let ordenesClientes = [];
-
-// Función y prompt
-while (true) {
-  let tipoCafe = prompt("¿Qué tipo de café quieres? Americano, Expresso, Cappuccino o Latte. (Haz click en cancelar para salir)");
-  if (!tipoCafe) {
-    break;
-  }
-  let cantidadCafe = parseInt(prompt(`¿Cuántos quieres ordenar?`));
-  if (!isNaN(cantidadCafe)) {
-    let nombreCliente = prompt("¿Cuál es tu nombre?");
-    ordenesClientes.push({nombre: nombreCliente, tipoCafe: tipoCafe, cantidad: cantidadCafe});
-  }
-}
-
-// Map y array
-const preciosOrdenes = ordenesClientes.map(orden => {
-  const tipoDeCafe = cafeTipos.find(cafe => cafe.nombre.toLowerCase() === orden.tipoCafe.toLowerCase());
-  if (tipoDeCafe) {
-    const precioTotal = tipoDeCafe.precio * orden.cantidad;
-    return {
-      // datos ingresados en el prompt
-      cliente: orden.nombre,
-      tipoCafe: tipoDeCafe.nombre,
-      cantidad: orden.cantidad,
-      precioTotal: precioTotal
-    };
-  } else {
-    return null;
-  }
-}).filter(orden => orden !== null);
-
-// mensaje con el detalle y precio final
-preciosOrdenes.forEach(orden => {
-  console.log(`Gracias ${orden.cliente}, el precio total por ${orden.cantidad} ${orden.tipoCafe} es: $${orden.precioTotal}`);
-});
-
-// console.log para verificar que funcione la info del .map
-console.log(preciosOrdenes);
-
-
-// Prompt para filtrar ordenes
-const tipoCafeFiltro = prompt("¿Quieres filtrar las órdenes por un tipo de café en específico? Ingresa el nombre del tipo de café o presiona 'cancelar' para continuar sin filtrar.");
-
-// Si el usuario ingreso un tipo de café para filtrar
-if (tipoCafeFiltro) {
-
-  const tipoCafeFiltroMin = tipoCafeFiltro.toLowerCase();
-
-  // Filtrar las ordenes
-  const ordenesFiltradas = preciosOrdenes.filter(orden => orden.tipoCafe.toLowerCase() === tipoCafeFiltroMin);
-
-  // Pintar las ordenes filtradas
-  console.log(`Órdenes de ${tipoCafeFiltro}:`);
-  ordenesFiltradas.forEach(orden => {
-    console.log(`- Cliente: ${orden.cliente}, Cantidad: ${orden.cantidad}, Precio Total: $${orden.precioTotal}`);
+fetch('assets/data/productos.json')
+  .then(response => response.json())
+  .then(data => {
+    sPhoneData = data;
+    muestraDatos();
+  })
+  .catch(error => {
+    console.error('Error json:', error);
   });
+  
+
+function muestraDatos() {
+  let gridProductos = document.querySelector(".grid-productos");
+  let fragment = document.createDocumentFragment();
+
+  // Itera sobre los productos
+  sPhoneData.forEach(item => {
+    let productCard = document.createElement("div");
+    let precioFormateado = item.precio.toFixed(0).replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+    let ofertaFormateada = item.oferta.toFixed(0).replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+
+    productCard.innerHTML =  `
+      <div class="product-card uk-card uk-light uk-relative">
+        <span class="sku">${item.id}</span>
+        <div class="marca uk-text-small">${item.marca}</div>
+        <h3 class="modelo uk-card-title uk-margin-remove">${item.modelo}</h3>
+        <div class="uk-text-small atributos">
+          <span class="color">${item.color}</span>
+          <span class="capacidad">${item.capacidad}</span>
+        </div>
+        <div class="oferta">$${ofertaFormateada}</div>
+        <div class="uk-text-small">$${precioFormateado}</div>
+        <button class="btn-agrega uk-button-default uk-padding-small uk-width-1-1 uk-margin-top">Agregar al carrito</button>
+      </div>`;
+
+    // Agrega html de producto a fragment
+    fragment.appendChild(productCard);
+    // console.log(sPhoneData);
+  });
+
+  // Limpia y agrega el fragmento al contenedor
+  gridProductos.innerHTML = "";
+  gridProductos.appendChild(fragment);
+
+  // Barra de buscar
+  let searchBox = document.querySelector(".uk-search-input");
+  let searchForm = document.querySelector(".uk-search");
+
+  searchBox.addEventListener("input", () => {
+    performSearch();
+  });
+
+  searchForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    performSearch();
+  });
+
+  function performSearch() {
+    let searchTerm = searchBox.value.toLowerCase();
+    let productCards = document.querySelectorAll(".grid-productos .product-card");
+
+    // Itera sobre html de producto, muestra/ocults segun la busqueda
+    productCards.forEach(card => {
+      let marca = card.querySelector(".product-card .marca").textContent.toLowerCase();
+      let modelo = card.querySelector(".product-card .modelo").textContent.toLowerCase();
+      let color = card.querySelector(".product-card .color").textContent.toLowerCase();
+      let id = card.querySelector(".product-card .sku").textContent.toLowerCase();
+
+      if (marca.includes(searchTerm) || modelo.includes(searchTerm) || color.includes(searchTerm) || id.includes(searchTerm)) {
+        card.classList.remove("hidden");
+      } else {
+        card.classList.add("hidden");
+      }
+    });
+  }
+
+  // Manejo del btn "agregar al carrito"
+  let addToCartButtons = document.querySelectorAll(".btn-agrega");
+
+  addToCartButtons.forEach(button => {
+    button.addEventListener("click", () => {
+      let productCard = button.closest(".product-card");
+      let sku = productCard.querySelector(".sku").textContent;
+      let marca = productCard.querySelector(".marca").textContent;
+      let modelo = productCard.querySelector(".modelo").textContent;
+      let color = productCard.querySelector(".color").textContent;
+      let oferta = productCard.querySelector(".oferta").textContent;
+
+      let carritoInfo = document.createElement("div");
+      carritoInfo.classList.add("info");
+      
+      carritoInfo.innerHTML = `
+        <div class="marca">${marca}</div>
+        <div class="modelo">${modelo}</div>
+        <div class="color">${color}</div>
+        <div class="oferta">${oferta}</div>
+        <button class="btn-quitar uk-button-default uk-width-1-1 uk-margin-top">Quitar</button>`;
+
+      // Agrega la informacion del producto al carrito
+      document.querySelector("#carrito").appendChild(carritoInfo);
+
+      // Agrega el producto al arreglo de datos carrito
+      carritoData.push({ sku, marca, modelo, color, oferta });
+
+      // Actualiza el carrito y guarda datoss
+      updateCarrito();
+      saveCarritoData();
+
+      let removeButton = carritoInfo.querySelector(".btn-quitar");
+      removeButton.addEventListener("click", () => {
+        // Quita la informacion del producto carrito
+        carritoInfo.remove();
+
+        // Encuentra el indice del producto en el arreglo y lo quita
+        let index = carritoData.findIndex(product => product.sku === sku);
+        if (index !== -1) {
+          carritoData.splice(index, 1);
+        }
+
+        // Actualiza el carrito y guarda los datos
+        updateCarrito();
+        saveCarritoData();
+      });
+    });
+  });
+
+  function updateCarrito() {
+    // Actualiza el contenido del carrito con los datos carritoData
+  }
+
+  function saveCarritoData() {
+    // Guarda datos del carrito en local storage
+    localStorage.setItem("carritoData", JSON.stringify(carritoData));
+  }
+
+  function loadCarritoData() {
+    let savedData = localStorage.getItem("carritoData");
+    if (savedData) {
+      carritoData = JSON.parse(savedData);
+      carritoData.forEach(product => {
+        let carritoInfo = document.createElement("div");
+        carritoInfo.classList.add("info");
+        
+        carritoInfo.innerHTML = `
+        <div class="marca">${product.marca}</div>
+        <div class="modelo">${product.modelo}</div>
+        <div class="color">${product.color}</div>
+        <div class="oferta">${product.oferta}</div>
+        <button class="btn-quitar uk-button-default uk-width-1-1 uk-margin-top">Quitar</button>`;
+
+        // Agrega la informacion del producto al carrito
+        document.querySelector("#carrito").appendChild(carritoInfo);
+
+        let removeButton = carritoInfo.querySelector(".btn-quitar");
+        removeButton.addEventListener("click", () => {
+          // Elimina la informacion del producto del carrito
+          carritoInfo.remove();
+
+          // Encuentra el indice del producto en el arreglo y lo quita
+          let index = carritoData.findIndex(p => p.sku === product.sku);
+          if (index !== -1) {
+            carritoData.splice(index, 1);
+          }
+
+          // Actualiza carrito y guarda datos
+          updateCarrito();
+          saveCarritoData();
+        });
+      });
+    }
+  }
+
+
+  loadCarritoData();
 }
